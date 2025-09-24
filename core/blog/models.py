534644b53454d6bd32d01674e2a54873a33904f1
mzulfikari <mzulfikari1382@gmail.com>
2025-09-24 20:15:35 +0330

    init

---
+++ core/blog/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Post(models.Model):
+    """
+    this is a class to define posts for blog app
+    """
+
+    author = models.ForeignKey("accounts.Profile", on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def get_snippet(self):
+        return self.content[0:5]
+
+    def get_absolute_api_url(self):
+        return reverse("blog:api-v1:post-detail", kwargs={"pk": self.pk})
+
+
+class Category(models.Model):
+    """
+    this is a class to define categories for blog table
+    """
+
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
